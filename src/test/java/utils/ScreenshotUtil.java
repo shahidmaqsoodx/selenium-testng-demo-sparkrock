cd3a82f0eeef20fbd16410f35aace4b9c4486449
Shahid Maqsood <shahidmaq786@gmail.com>
2025-09-26 21:12:15 +0500

    add: Screenshot util file to add screenshots functionality on failure

---
+++ src/test/java/utils/ScreenshotUtil.java
@@ -0,0 +1,31 @@
+package utils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ScreenshotUtil {
+
+    public static void takeScreenshot(WebDriver driver, String testName) {
+        try {
+            TakesScreenshot ts = (TakesScreenshot)driver; //Down casting
+        	File src = ts.getScreenshotAs(OutputType.FILE); 
+
+            // Ensure screenshots folder exists
+            String folderPath = "screenshots/";
+            Files.createDirectories(Paths.get(folderPath));
+
+            String filePath = folderPath + testName + "_" + System.currentTimeMillis() + ".png";
+            File dest = new File(filePath);
+            Files.copy(src.toPath(), dest.toPath()); //Copy screenshot to the created screenshots folder
+
+            System.out.println("Screenshot saved: " + filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
